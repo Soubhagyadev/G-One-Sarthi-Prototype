05a2401f59527c4d689697257738fee8813b9d63
Soubhagyadev <soubhagyaswain141@gmail.com>
2026-09-12 07:31:21 +0530

    App Icon

--- app.config.js
+++ app.config.js
@@ -4,10 +4,17 @@ module.exports = {
     slug: 'g-one-sarthi',
     version: '1.0.0',
     orientation: 'portrait',
+    icon: './assets/icon.png',
     userInterfaceStyle: 'light',
     androidNavigationBar: {
       backgroundColor: '#F9F6F0',
       barStyle: 'dark-content',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/icon.png',
+        backgroundColor: '#F9F6F0',
+      },
     },
     plugins: ['@react-native-community/datetimepicker'],
     extra: {
